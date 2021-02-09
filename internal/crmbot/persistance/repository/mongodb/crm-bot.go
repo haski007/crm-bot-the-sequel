d3325d1223c1cd2c0602f8e1b14d338673b205d9
Haski007 <haski007001@gmail.com>
2021-02-09 22:41:34 +0200

    feat: changed database on Atlas.Mongo

--- internal/crmbot/persistance/repository/mongodb/crm-bot.go
+++ internal/crmbot/persistance/repository/mongodb/crm-bot.go
@@ -1,6 +1,9 @@
 package mongodb
 
 import (
+	"crypto/tls"
+	"net"
+
 	"github.com/Haski007/crm-bot-the-sequel/internal/crmbot/config"
 	"github.com/caarlos0/env"
 	"github.com/globalsign/mgo"
@@ -15,12 +18,28 @@ func init() {
 		logrus.Fatalf("[env Parse] MongoCfg err: %s", err)
 	}
 
+	tlsConfig := &tls.Config{}
+
+	dialInfo := &mgo.DialInfo{
+		Addrs: []string{"cluster0-shard-00-00.k2lrx.mongodb.net:27017",
+			"cluster0-shard-00-01.k2lrx.mongodb.net:27017",
+			"cluster0-shard-00-02.k2lrx.mongodb.net:27017"},
+		Database: "admin",
+		Username: cfg.Username,
+		Password: cfg.Password,
+	}
+
+	dialInfo.DialServer = func(addr *mgo.ServerAddr) (net.Conn, error) {
+		conn, err := tls.Dial("tcp", addr.String(), tlsConfig)
+		return conn, err
+	}
+
 	var err error
-	cfg.Addr = "mongodb://" + cfg.Username + ":" + cfg.Password + "@" + cfg.HostName + ":" + cfg.Port
-	session, err = mgo.Dial(cfg.Addr)
+	session, err = mgo.DialWithInfo(dialInfo)
 	if err != nil {
 		logrus.Fatalf("[mgo Dial] addr: %s | err: %s", cfg.Addr, err)
 	}
+	session.SetMode(mgo.PrimaryPreferred, false)
 
 	if err = session.Ping(); err != nil {
 		logrus.Fatalf("[mgo Ping] addr: %s | err: %s", cfg.Addr, err)
